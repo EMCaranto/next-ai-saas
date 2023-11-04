22397d07e74a8d8dfc15cf30e16bdb717d9cfcb2
Erickson Caranto <erickson.caranto.m@gmail.com>
2023-11-04 15:50:21 +0800

    :hammer: Added code generator AI API (OpenAI)

---
+++ app/api/code/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server'
+
+// Dependencies
+import { auth } from '@clerk/nextjs'
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: 'system',
+  content: 'You are a code generator. You must asnwer only in a markdown code snippets. Use the code comments for explanations'
+}
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = auth()
+
+    const body = await request.json()
+    const { messages } = body
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse('OpenAI API key not configured', { status: 500 })
+    }
+
+    if (!messages) {
+      return new NextResponse('Message is required', { status: 400 })
+    }
+
+    const response = await openai.createChatCompletion({
+      model: 'gtp-3.5-turbo',
+      messages: [instructionMessage, ...messages]
+    })
+
+    return NextResponse.json(response.data.choices[0].message)
+  }
+  catch (error) {
+    console.log('[CODE_GENERATION_ERROR]', error)
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
